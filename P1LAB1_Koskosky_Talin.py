460a537783f049999ecb4becd70f3f5de317aecc
TalinKoskosky <koskoskt0239@student.faytechcc.edu>
2024-10-12 19:01:00 -0400

    Add files via upload

---
+++ P1LAB1_Koskosky_Talin.py
@@ -0,0 +1,14 @@
+#Talin Koskosky
+#10/8/24\
+#P1LAB1
+#This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+
+#Use first and last name
+first_name = input('enter your first name')
+Last_name = input('enter your last name')
+print()
+print()
+print()
+print(f"Hello, {first_name} {Last_name} welcome to CTI-110")
+    
+
